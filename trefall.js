bd1abfb7f0c7b525d4c2af95d60bcee8ec4ba1e5
Jesse Chen <jesse.z.chen@gmail.com>
2026-02-12 15:41:58 -0500

    Basic operations

--- trefall.js
+++ trefall.js
@@ -1,1 +1,33 @@
-console.log('It works!');
+class Grid {
+    constructor(side) {
+        this.side = side;
+        this.internal = [];
+        for (let i = 0; i < side * 2; i++) {
+            this.internal[i] =  new Array(side * 4).fill(0);
+        }
+    }
+
+    get(x, y, w) {
+        return this.internal[y][x * 2 + w];
+    }
+
+    set(value, x, y, w) {
+        this.internal[y][x * 2 + w] = value;
+    }
+
+    isInside(x, y, w) {
+        const q = x + y;
+        if (q < this.side - 1) { return false; }
+        if (q === this.side - 1) { return !!w; }
+        if (q > this.side * 3 - 1) { return false; }
+        if (q === this.side * 3 - 1) { return !w; }
+        return true;
+    }
+}
+
+const grid = new Grid(3);
+grid.set(5, 2, 0, 0);
+grid.set(4, 2, 0, 1);
+console.log(grid.internal[0]);
+console.log(grid.isInside(2, 0, 0));
+console.log(grid.isInside(2, 0, 1));
