73238f0fb04e1d3604bae6f1e469896819628542
Jesse Chen <jesse.z.chen@gmail.com>
2026-02-12 16:15:39 -0500

    Add coords class

--- trefall.js
+++ trefall.js
@@ -1,3 +1,15 @@
+class Coords {
+    constructor(x, y, w) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+    }
+
+    toString() {
+        return `(${this.x}, ${this.y}, ${this.w})`;
+    }
+}
+
 class Grid {
     constructor(side) {
         this.side = side;
@@ -7,49 +19,49 @@ class Grid {
         }
     }
 
-    get(x, y, w) {
-        if (!this.isInside(x, y, w)) {
-            console.error(`Getting (${x}, ${y}, ${w}) is not inside!`)
+    get(coords) {
+        if (!this.isInside(coords)) {
+            console.error(`Getting ${coords} is not inside!`)
             return 0;
         }
         return this.internal[y][x * 2 + w];
     }
 
-    set(value, x, y, w) {
-        if (!this.isInside(x, y, w)) {
-            console.error(`Setting (${x}, ${y}, ${w}) is not inside!`)
+    set(value, coords) {
+        if (!this.isInside(coords)) {
+            console.error(`Setting ${coords} is not inside!`)
             return 0;
         }
-        this.internal[y][x * 2 + w] = value;
+        this.internal[coords.y][coords.x * 2 + coords.w] = value;
     }
 
-    isInside(x, y, w) {
-        const q = x + y;
+    isInside(coords) {
+        const q = coords.x + coords.y;
         if (q < this.side - 1) { return false; }
-        if (q === this.side - 1) { return !!w; }
+        if (q === this.side - 1) { return !!coords.w; }
         if (q > this.side * 3 - 1) { return false; }
-        if (q === this.side * 3 - 1) { return !w; }
+        if (q === this.side * 3 - 1) { return !coords.w; }
         return true;
     }
 
-    neighbors(x, y, w) {
+    neighbors(coords) {
         const result = [];
-        if (!this.isInside(x, y, w)) {
-            console.error(`Neighborhood of (${x}, ${y}, ${w}) is not inside!`)
+        if (!this.isInside(coords)) {
+            console.error(`Neighborhood of ${coords} is not inside!`)
             return result;
         }
-        if (!!w) {
-            if (this.isInside(x, y, 0)) { result.push(x, y, 0); }
-            if (this.isInside(x + 1, y, 0)) { result.push(x + 1, y, 0); }
-            if (this.isInside(x, y + 1, 0)) { result.push(x, y + 1, 0); }
+        if (!!coords.w) {
+            if (this.isInside(new Coords(coords.x, coords.y, 0))) { result.push(new Coords(coords.x, coords.y, 0)); }
+            if (this.isInside(new Coords(coords.x + 1, coords.y, 0))) { result.push(new Coords(coords.x + 1, coords.y, 0)); }
+            if (this.isInside(new Coords(coords.x, coords.y + 1, 0))) { result.push(new Coords(coords.x, coords.y + 1, 0)); }
         } else {
-            if (this.isInside(x, y, 1)) { result.push(x, y, 1); }
-            if (this.isInside(x - 1, y, 1)) { result.push(x - 1, y, 1); }
-            if (this.isInside(x, y - 1, 1)) { result.push(x, y - 1, 1); }
+            if (this.isInside(new Coords(coords.x, coords.y, 1))) { result.push(new Coords(coords.x, coords.y, 1)); }
+            if (this.isInside(new Coords(coords.x - 1, coords.y, 1))) { result.push(new Coords(coords.x - 1, coords.y, 1)); }
+            if (this.isInside(new Coords(coords.x, coords.y - 1, 1))) { result.push(new Coords(coords.x, coords.y - 1, 1)); }
         }
         return result;
     }
 }
 
 const grid = new Grid(3);
-console.log(grid.neighbors(2, 0, 1));
+console.log(grid.neighbors(new Coords(2, 0, 1)));
